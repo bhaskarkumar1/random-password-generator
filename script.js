8a7f5817302718fed6d9337289742c3f2c43a558
Bhaskar <bhaskarkumar.india@gmail.com>
2023-12-26 12:22:16 +0530

    check for mobile  screen

---
+++ script.js
@@ -0,0 +1,7 @@
+upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower="abcdefghijklmnopqrstuvwxyz"
+number="0123456789"
+special="!@#$%^&*()-_=+"
+
+// randomize the upper  lower number and speacial 
+// generate random no .
